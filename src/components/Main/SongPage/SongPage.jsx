048dc9f65c397c0ff56c7a1a88bc6e1654c5c3ec
Sebastian Westerholm <westerholm.sebastian@gmail.com>
2025-06-05 00:43:01 +0200

    Wip: Song page

--- src/components/Main/SongPage/SongPage.jsx
+++ src/components/Main/SongPage/SongPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { skeletaLyrics } from "../../../../public/data/skeletaLyrics";
 import "./SongPageStyle.css";
@@ -14,8 +15,28 @@ function SongPage() {
     // FIXA: Snygg layout för text/video/tabs
     // If för intro, pre-chorus osv?
     <>
-      <h1 style={{ color: "white" }}>{`${track} from ${name}`}</h1>
-      <div style={{ color: "white" }}>{lyric}</div>
+      <h1 style={{ color: "white" }} className="song-title">{`${track} `}</h1>
+      <div className="song-container">
+        <div style={{ color: "white" }} className="lyrics-container">
+          {lyric.map((section, sectionIndex) => (
+            <div key={sectionIndex} style={{ marginBottom: "1em" }}>
+              {section.map((line, lineIndex) => (
+                <p key={lineIndex}>{line}</p>
+              ))}
+            </div>
+          ))}
+        </div>
+
+        <div className="tabs-container">
+          <div>Guitar tabs</div>
+          <div>Bass tabs</div>
+          {/* Embedded video ? */}
+        </div>
+      </div>
+
+      {/* FIXA STYLING */}
+      <Link to={`/${name}`}>Back to {name}</Link>
+      <Link to={`/`}>Back to start</Link>
     </>
   );
 }
